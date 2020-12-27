00568a3045e637934c25c8f8efd00d02351bf5f7
Sandeep Kandula <sandeep.kandula@in.abb.com>
2020-12-27 17:55:58 +0530

    First draft of working of SQL Server. PostgreSQL

---
+++ 2/PostgreSQL/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    //DbContext: Change tracking
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
